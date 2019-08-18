9965fef1b3b3271f6b1c3ce458b4ff5074536cc2
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2019-08-18 17:27:36 +0200

    First display of table.

---
+++ src/components/ListSpendings.vue
@@ -0,0 +1,29 @@
+<template>
+    <table class="table" v-if="spendings.length > 0">
+        <thead>
+            <tr>
+                <th scope="col">Price</th>
+                <th scope="col">Desc.</th>
+            </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(spending, idx) in spendings" :key="idx">
+            <td>{{spending.price}}</td>
+            <td>{{spending.desc}}</td>
+          </tr>
+        </tbody>
+    </table>
+    <div v-else>No spending to display.</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      spendings: [
+        
+      ]
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,2 +1,16 @@
 <template>
+  <div class="home">
+    <h1>Home</h1>
+    <list-spendings />
+  </div>
 </template>
+
+<script>
+import ListSpendings from '@/components/ListSpendings.vue'
+export default {
+  name: 'home',
+  components: {
+    ListSpendings
+  }
+}
+</script>
